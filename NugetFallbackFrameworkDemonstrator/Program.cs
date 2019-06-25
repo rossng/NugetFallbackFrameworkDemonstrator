9d8c3d5fae644c56e5f9f2cb954b1dda2106b0c6
Ross Gardiner <Ross.Gardiner@3shape.com>
2019-06-25 15:50:45 +0200

    Initial commit

---
+++ NugetFallbackFrameworkDemonstrator/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NuGet.ProjectModel;
+using System.IO;
+
+namespace NugetFallbackFrameworkDemonstrator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var dependencyGraph = ReadDependencyGraph(args[0]);
+        }
+
+        public static DependencyGraphSpec ReadDependencyGraph(string dependencyGraphPath)
+        {
+            using (var file = File.OpenText(dependencyGraphPath))
+            using (var reader = new JsonTextReader(file))
+            {
+                return new DependencyGraphSpec((JObject)JToken.ReadFrom(reader));
+            }
+        }
+    }
+}
